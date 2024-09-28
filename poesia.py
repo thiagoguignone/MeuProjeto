271a71707fdc937faa48eb15c975277b8998b990
Thiago Guignone <guignonet@gmail.com>
2024-09-28 16:51:38 -0300

    Adicionando poesia.py no projeto

---
+++ poesia.py
@@ -0,0 +1,5 @@
+#poesia Soneto de Fidelidade
+print("De tudo, ao meu amor serei atento")
+print("Antes, e com tal zelo, e sempre, e tanto")
+print("Que mesmo em face do maior encanto")
+print("Dele se encante mais meu pensamento!")
